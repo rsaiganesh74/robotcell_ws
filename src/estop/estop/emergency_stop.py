38e9753198d07f5da3615d123c00330553ddc60f
Sai Ganesh Raju <rsaiganesh74@gmail.com>
2025-02-23 00:52:01 +0100

    Initial working version of estop node

---
+++ src/estop/estop/emergency_stop.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Bool
+from std_srvs.srv import SetBool
+
+class Estop(Node):
+    def __init__(self):
+        super().__init__('estop_node')
+        
+        self.publish_interval = 0.01 #rate at which the publisher published the barcode number
+
+        self.pub = self.create_publisher(Bool,'estop_status',10) #keeping 10 as depth so that subscriber has access to 10 last numbers in case of delay
+
+        self.create_service(SetBool,'estop_status_set',self.cb_estop_request)
+
+        self.timer = self.create_timer(self.publish_interval,self.cb_publish)
+
+        self.estop_status_msg = Bool() #defining the msg here itself to avoid redefinition in publisher callback constantly
+
+        self.estop_state = False #assuming estop is not pressed initially
+
+    def cb_publish(self):
+        self.estop_status_msg.data = self.estop_state
+        self.pub.publish(self.estop_status_msg)
+
+    def cb_estop_request(self,request,response):
+        if self.estop_state:
+            if request.data:
+                response.success = False
+                response.message = "Failed to execute request. Emergency Stop button is already pressed."
+            else:
+                response.success = True
+                response.message = "Request successfully executed. Emergency Stop button is disengaged now."
+        else:
+            if request.data:
+                response.success = True
+                response.message = "Request successfully executed. Emergency Stop button is now pressed."
+            else:
+                response.success = False
+                response.message = "Failed to execute request. Emergency Stop button is already disengaged."
+        if response.success:
+            self.estop_state = not self.estop_state
+
+        self.get_logger().info(f'{response.message}')
+        return response
+
+def main():
+    rclpy.init()
+
+    estop = Estop()
+
+    try:
+        rclpy.spin(estop)
+    except KeyboardInterrupt:
+        pass
+
+if __name__=="__main__":
+    main()
---
+++ src/estop/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'estop'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Sai Ganesh Raju',
+    maintainer_email='rsaiganesh74@gmail.com',
+    description='TODO: This package mimics an emergency stop',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'estop_node = estop.emergency_stop:main',
+        ],
+    },
+)
